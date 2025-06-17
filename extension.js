dad05a2f833651ba81cba795ad7b0d4fdfe0efc3
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-06-17 09:02:52 +0000

    feat: dynamically check and display OpenAFS client status on menu open
    The extension now updates the client status label each time the user opens the dropdown,
    allowing it to reflect external changes made from the command line.

--- extension.js
+++ extension.js
@@ -18,56 +18,88 @@
 
 import GObject from 'gi://GObject';
 import St from 'gi://St';
+import GLib from 'gi://GLib';
 
 import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
-
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 const Indicator = GObject.registerClass(
-  class Indicator extends PanelMenu.Button {
-    _init() {
-      super._init(0.0, _('OpenAFS Status'));
+class Indicator extends PanelMenu.Button {
+  _init() {
+    super._init(0.0, _('OpenAFS Status'));
 
-      const icon = new St.Icon({
-        icon_name: 'network-wired-symbolic',
-        style_class: 'system-status-icon',
-      });
-      this.add_child(icon);
+    const icon = new St.Icon({
+      icon_name: 'network-wired-symbolic',
+      style_class: 'system-status-icon',
+    });
+    this.add_child(icon);
 
-      // Start/Stop AFS Client
-      this._startItem = new PopupMenu.PopupMenuItem(_('Start AFS Client'));
-      this._stopItem = new PopupMenu.PopupMenuItem(_('Stop AFS Client'));
-      this._stopItem.setSensitive(false); // Initially disabled
-      this.menu.addMenuItem(this._startItem);
-      this.menu.addMenuItem(this._stopItem);
+    // Start/Stop AFS Client
+    this._startItem = new PopupMenu.PopupMenuItem(_('Start AFS Client'));
+    this._stopItem = new PopupMenu.PopupMenuItem(_('Stop AFS Client'));
+    this.menu.addMenuItem(this._startItem);
+    this.menu.addMenuItem(this._stopItem);
 
-      // Token Status
-      this._tokenStatusItem = new PopupMenu.PopupMenuItem(_('Token: Not Available'));
-      this.menu.addMenuItem(this._tokenStatusItem);
+    // Token Status
+    this._tokenStatusItem = new PopupMenu.PopupMenuItem(_('Token: Not Available'));
+    this.menu.addMenuItem(this._tokenStatusItem);
 
-      // Client Status
-      this._clientStatusItem = new PopupMenu.PopupMenuItem(_('Client: Not Running'));
-      this.menu.addMenuItem(this._clientStatusItem);
+    // Client Status
+    this._clientStatusItem = new PopupMenu.PopupMenuItem(_('Client: Checking...'));
+    this.menu.addMenuItem(this._clientStatusItem);
 
-      // Connect signals
-      this._startItem.connect('activate', () => {
-        log('Start AFS Client');
-        this._startItem.setSensitive(false);
-        this._stopItem.setSensitive(true);
-        this._clientStatusItem.label.set_text(_('Client: Running'));
-      });
+    // Connect signals
+    this._startItem.connect('activate', () => {
+      log('Start AFS Client');
+      // UI simulation only
+      this._startItem.setSensitive(false);
+      this._stopItem.setSensitive(true);
+      this._clientStatusItem.label.set_text(_('Client: Running'));
+    });
 
-      this._stopItem.connect('activate', () => {
-        log('Stop AFS Client');
-        this._startItem.setSensitive(true);
-        this._stopItem.setSensitive(false);
-        this._clientStatusItem.label.set_text(_('Client: Not Running'));
-      });
+    this._stopItem.connect('activate', () => {
+      log('Stop AFS Client');
+      this._startItem.setSensitive(true);
+      this._stopItem.setSensitive(false);
+      this._clientStatusItem.label.set_text(_('Client: Not Running'));
+    });
+
+    // Dynamic update on menu open
+    this.menu.connect('open-state-changed', (menu, isOpen) => {
+      if (isOpen) {
+        this._updateClientStatus();
+      }
+    });
+
+    // Initial status check
+    this._updateClientStatus();
+  }
+
+  _updateClientStatus() {
+    try {
+      let [ok, out] = GLib.spawn_command_line_sync('systemctl is-active openafs-client');
+      if (ok) {
+        let result = out.toString().trim();
+        if (result === 'active') {
+          this._clientStatusItem.label.set_text(_('Client: Running'));
+          this._startItem.setSensitive(false);
+          this._stopItem.setSensitive(true);
+        } else {
+          this._clientStatusItem.label.set_text(_('Client: Not Running'));
+          this._startItem.setSensitive(true);
+          this._stopItem.setSensitive(false);
+        }
+      } else {
+        this._clientStatusItem.label.set_text(_('Client: Unknown'));
+      }
+    } catch (e) {
+      logError(e);
+      this._clientStatusItem.label.set_text(_('Client: Error'));
     }
   }
-);
+});
 
 export default class OpenAFSStatusExtension extends Extension {
   enable() {
